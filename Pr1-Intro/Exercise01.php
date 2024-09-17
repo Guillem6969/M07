72760a7039ecbae8b360599c58edc87066424c34
Guillem6969 <guim.san.oli@gmail.com>
2024-09-17 18:13:38 +0200

    gg

---
+++ Pr1-Intro/Exercise01.php
@@ -0,0 +1,21 @@
+<?php
+// Creamos las variables numéricas
+$num1 = 10;
+$num2 = 5;
+
+// Realizar las operaciones
+$suma = $num1 + $num2;
+$resta = $num1 - $num2;
+$division = $num1 / $num2;
+
+// Mostrar los valores de las variables y los resultados
+echo "Valor de num1: $num1<br>";
+echo "Valor de num2: $num2<br>";
+
+echo "Suma: $num1 + $num2 = $suma<br>";
+echo "Resta: $num1 - $num2 = $resta<br>";
+echo "División: $num1 / $num2 = $division<br>";
+
+if ($num1 == $num2) {
+    echo "";
+}
