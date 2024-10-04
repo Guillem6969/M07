96dddf6a922b8693d5d8503772c7b832b48a6750
Guillem6969 <guim.san.oli@gmail.com>
2024-10-04 18:56:58 +0200

    guillem

---
+++ Pr1-Intro/ExerciseSessions01.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form method="GET">
+
+        <h1>Supermarket management</h1>
+
+            <p>Worker name:<input type="text" name="nombre" id=""></p>
+            <br>
+        <h2>Choose Product:</h2>
+    
+            <select name="" id="">
+                <option value="">MILK</option>
+                <option value="" >SOFT DRINK</option>
+            </select>
+            <br>
+            <br>
+        <h2>Product quantity:</h2>
+
+        <input type="text" name="cantidad" id="">
+
+        <br>
+        <button>add</button>
+        <button>remove</button>
+        <button>reset</button>
+
+        <br>
+
+        <h2>Inventary:</h2>
+
+    </form>
+</body>
+
+</html>
+
+<?php
+
+session_start();
+
