942d4b8ba8b627eae93c8a307c16452bdb86b4e3
Guillem6969 <guim.san.oli@gmail.com>
2024-10-06 22:57:41 +0200

    SESION

--- Pr1-Intro/ExerciseSessions01.php
+++ Pr1-Intro/ExerciseSessions01.php
@@ -4,37 +4,8 @@ b) Se debe poder añadir y quitar leche o refresco seleccionando de una lista
 c) Se debe controlar que no se pueden quitar mas unidades de las que haya, en ese 
 caso mostrar error */
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="ExerciseSessions01.php" method="post">
-        <h1>Supermarket management</h1>
-        Worker name:<input type="text" name="name" required>
-        <h2>Choose product:</h2>
-        <select name="product">
-            <option value="milk">Milk</option>
-            <option value="soft_drink">Soft drink</option>
-        </select>
-        <h2>Product quantity:</h2>
-        <input type="number" name="quantity" required>
-        <br><br>    
-        <input type="submit" name="add" value="add">
-        <input type="submit" name="remove" value="remove">
-        <input type="reset" name="reset" value="reset">
-    </form>
-    <h3>Inventary:</h3>
-    <p>Worker: <?php echo $_SESSION["name"]?></p>
-    <p>Units milk: <?php echo $_SESSION["quantity_milk"] ?></p>
-    <p>Units soft drink: <?php echo $_SESSION["quantity_softdrink"] ?></p>
-</body>
-</html>
-
 <?php
+
 session_start();
 
 if (!isset($_SESSION["quantity_milk"])) { 
@@ -74,3 +45,34 @@ if (isset($_POST["add"])) {
         }
     }
 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="ExerciseSessions01.php" method="post">
+        <h1>Supermarket management</h1>
+        Worker name:<input type="text" name="name" required>
+        <h2>Choose product:</h2>
+        <select name="product">
+            <option value="milk">Milk</option>
+            <option value="soft_drink">Soft drink</option>
+        </select>
+        <h2>Product quantity:</h2>
+        <input type="number" name="quantity" required>
+        <br><br>    
+        <input type="submit" name="add" value="add">
+        <input type="submit" name="remove" value="remove">
+        <input type="reset" name="reset" value="reset">
+    </form>
+    <h3>Inventary:</h3>
+    <p>Worker: <?php echo $_SESSION["name"]?></p>
+    <p>Units milk: <?php echo $_SESSION["quantity_milk"] ?></p>
+    <p>Units soft drink: <?php echo $_SESSION["quantity_softdrink"] ?></p>
+</body>
+</html>
+
