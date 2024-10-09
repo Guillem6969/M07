97cec066e08cc5c92300787788531c1e2e183e1e
Guillem6969 <guim.san.oli@gmail.com>
2024-10-09 20:05:05 +0200

    BANCO

--- Pr2-POO/BANK.php
+++ Pr2-POO/BANK.php
@@ -9,8 +9,8 @@
 <?php
 
 $first_account_balance = 400.0;
-$first_account_status = "closed";
-$first_account_overdraft = false;
+$first_account_status = "opened";
+$first_account_overdraft = 0;
 
 echo "My balance : " . $first_account_balance .".<br>";
 
@@ -33,8 +33,8 @@ echo "My new balance after deposit (+150) : ". $first_account_balance ."<br>";
 echo "Doing transaction withdrawal (-600) with current balance ". $first_account_balance ."<br>";
 
 
-if (($first_account_balance - 600) < 0) {
-  $first_account_overdraft = true;
+if (($first_account_balance - 600) < $first_account_overdraft) {
+  
   echo "Error transaction : Insuficient balance to complete the withdrawal.<br>";
 
 }
@@ -46,5 +46,46 @@ $first_account_status = "closed";
 echo "My account is now ". $first_account_status .".<br>";
 
 
+echo"<br><br>-------------------------------------------------------- <br><br>";
+
+$second_account_balance = 200.0;
+$second_account_status = "opened";
+$second_account_overdraft = -100;
+
+echo "My balance : " . $second_account_balance .".<br>";
+
+echo "Doing transaction deposit (+100) with current balance ". $second_account_balance ."<br>";
+
+$second_account_balance += 100;
+echo "My new balance after deposit (+100) : ". $second_account_balance ."<br>";
 
 
+echo "Doing transaction withdrawl (-300) with current balance ". $second_account_balance ."<br>";
+
+$second_account_balance -= 300;
+echo "My new balance after deposit (-300) : ". $second_account_balance ."<br>";
+
+echo "Doing transaction withdrawl (-50) with current balance ". $second_account_balance ."<br>";
+
+$second_account_balance -= 50;
+echo "My new balance after deposit (-50) : ". $second_account_balance ."<br>";
+
+echo "Doing transaction withdrawl (-120) with current balance ". $second_account_balance ."<br>";
+
+if (($second_account_balance - 120) < $second_account_overdraft) {
+
+  echo "Error transaction : Withdrawal exceeds overdraft limit.<br>";
+
+}
+
+echo "My balance after failed last transaction : ". $second_account_balance ."<br>";
+
+echo "Doing transaction withdrawl (-20) with current balance ". $second_account_balance ."<br>";
+
+$second_account_balance -= 20;
+echo "My new balance after deposit (-300) : ". $second_account_balance ."<br>";
+
+$second_account_status = "closed";
+echo "My account is now ". $second_account_status .".<br>";
+
+echo "Error: Account is already closed.";
